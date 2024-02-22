62bf9f2105ca49904989f86ada82045cde9ee880
BrotherSharma <bhargavsharma1212@gmail.com>
2024-02-22 17:35:49 +0530

    dsa

--- MVC/Program.cs
+++ MVC/Program.cs
@@ -2,6 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IStudentRepositories,StudentRepositories>();
+builder.Services.AddSingleton<NpgsqlConnection>((serviceProvider) =>
+{
+    // Use the IConfiguration service to get the connection string
+    var connectionString = serviceProvider.GetRequiredService<IConfiguration>().GetConnectionString("DefaultConnection");
+    return new NpgsqlConnection(connectionString);
+});
 
 var app = builder.Build();
 
