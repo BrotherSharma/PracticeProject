20a895737af48635936097abe2b7d8302c29e3ab
Lal-Neha <lneha286@gmail.com>
2024-02-22 17:19:12 +0530

    tbluser

---
+++ MVC/Models/tbluser.cs
@@ -0,0 +1,13 @@
+namespace MVC.Models
+{
+    public class tbluser
+    {
+        public int Id {get; set;}
+        public string  UserName { get; set; }
+        public string EmailId {get; set;}
+        public string Password {get; set;}
+        
+        
+        
+    }
+}
