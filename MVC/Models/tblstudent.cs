eaf598d8ac945568573142ec780eab0b52f10315
shrestharani <kondabathinishrestharani@gmail.com>
2024-02-22 16:20:37 +0530

    tblstudent model changes all

--- MVC/Models/tblstudent.cs
+++ MVC/Models/tblstudent.cs
@@ -2,11 +2,15 @@ namespace MVC.Models
 {
     public class tblstudent
     {
-        public int id{get;set;}
-        public string Name{get;set;}
-        public string Gender{get;set;}
-
-        public string image{get;set;}
-        public string document{get; set;}
+        public int id{get; set;}
+        public string studentname { get; set; }
+        public string gender { get; set; }
+        public string address { get; set; }
+        public string language { get; set; }
+        public int course_id { get; set; }
+        public double phonenumber { get; set; }
+        public DateTime dbo { get; set; }
+        public string image { get; set; }
+        public string document { get; set; }
     }
 }
